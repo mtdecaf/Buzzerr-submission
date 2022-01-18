09af09c0d08dfb8e4a3f1c7434f15e2d67ea9ab8
Jacky Cao <jcvan0116@gmail.com>
2022-01-18 10:58:06 -0800

    school card styled

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -1,4 +1,5 @@
-
+import "./ProfileCard.scss";
+import send from "../../assets/images/send.png";
 
 const ProfileCard = ({ studentData }) => {
     console.log (studentData);
@@ -8,7 +9,7 @@ const ProfileCard = ({ studentData }) => {
             <h4 className="profile-card__name">{studentData.name}</h4>
             <p style={{backgroundColor: `${studentData.fieldColor}`}} className="profile-card__field">{studentData.field}</p>
             <p className="profile-card__mutual-interests">{studentData.mutualInterests} mutual interests</p>
-            <button className="profile-card__button"><img className="profile-card__button-icon" src="https://img.icons8.com/external-flatart-icons-outline-flatarticons/64/000000/external-send-email-flatart-icons-outline-flatarticons.png"/>Buzz</button>
+            <button className="profile-card__button"><img className="profile-card__button-icon" src={send}/>Buzz</button>
         </div>
     )
 }
